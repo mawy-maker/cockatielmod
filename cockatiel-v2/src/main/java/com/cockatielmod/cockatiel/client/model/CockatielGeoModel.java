0e52249469a3a75c8bf056f9180aea18bea9c731
mawy-maker <mawygarden@gmail.com>
2026-04-01 10:47:36 +0200

    Update CockatielGeoModel.java

--- cockatiel-v2/src/main/java/com/cockatielmod/cockatiel/client/model/CockatielGeoModel.java
+++ cockatiel-v2/src/main/java/com/cockatielmod/cockatiel/client/model/CockatielGeoModel.java
@@ -4,16 +4,17 @@ import com.cockatielmod.cockatiel.CockatielMod;
 import com.cockatielmod.cockatiel.entity.CockatielEntity;
 import net.minecraft.resources.ResourceLocation;
 import software.bernie.geckolib.model.GeoModel;
+import software.bernie.geckolib.renderer.GeoRenderer;
 
 public class CockatielGeoModel extends GeoModel<CockatielEntity> {
 
     @Override
-    public ResourceLocation getModelResource(CockatielEntity entity) {
+    public ResourceLocation getModelResource(CockatielEntity entity, GeoRenderer<CockatielEntity> renderer) {
         return CockatielMod.rl("geo/cockatiel.geo.json");
     }
 
     @Override
-    public ResourceLocation getTextureResource(CockatielEntity entity) {
+    public ResourceLocation getTextureResource(CockatielEntity entity, GeoRenderer<CockatielEntity> renderer) {
         return switch (entity.getVariant()) {
             case CockatielEntity.VARIANT_LUTINO -> CockatielMod.rl("textures/entity/cockatiel/cockatiel_lutino.png");
             case CockatielEntity.VARIANT_ALBINO -> CockatielMod.rl("textures/entity/cockatiel/cockatiel_albino.png");
